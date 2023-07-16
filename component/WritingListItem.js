93d276f7c1b12142f5c1619e7a6c49b115e183ca
C팀 <sywon2005@gmail.com>
2023-07-17 02:37:10 +0900

    몇분전 기능 구현

--- component/WritingListItem.js
+++ component/WritingListItem.js
@@ -4,10 +4,22 @@ import {Text, StyleSheet, Pressable, View} from 'react-native';
 import {formatDistanceToNow, format} from 'date-fns';
 import {ko} from 'date-fns/locale';
 
+export const formatDate = date => {
+  const d = new Date(date);
+  const now = Date.now();
+  const diff = (now - d.getTime()) / 1000;
+
+  if (diff < 60 * 1) return '방금전';
+  if (diff < 60 * 60 * 24 * 3)
+    return formatDistanceToNow(d, {addSuffix: true, locale: ko});
+
+  return format(d, 'PPP EEE p', {locale: ko});
+};
+
 const WritingListItem = ({data}) => {
   const navigation = useNavigation();
   const {item} = data;
-  const {title, body, date, nickName} = item;
+  const {title, body, date, nickName, id} = item;
 
   const onPress = () => {
     navigation.navigate('Detail', {
@@ -15,19 +27,8 @@ const WritingListItem = ({data}) => {
       body,
       date,
       nickName,
+      id,
     });
-  };
-
-  const formatDate = date => {
-    const d = new Date(date);
-    const now = Date.now();
-    const diff = (now - d.getTime()) / 1000;
-
-    if (diff < 60 * 1) return '방금전';
-    if (diff < 60 * 60 * 24 * 3)
-      return formatDistanceToNow(d, {addSuffix: true, locale: ko});
-
-    return format(d, 'PPP EEE p', {locale: ko});
   };
 
   return (
