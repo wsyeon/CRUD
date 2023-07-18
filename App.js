e892732e83a2b5000a9d8a52e55290df9ffa4a21
C팀 <sywon2005@gmail.com>
2023-07-18 21:44:04 +0900

    react-query 사용

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import RootStack from './screens/RootStack';
+import {SearchContextProvider} from './contexts/SearchContext';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <RootStack />
+      <SearchContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <RootStack />
+        </QueryClientProvider>
+      </SearchContextProvider>
     </NavigationContainer>
   );
 };
