f700b991f5af8af56b8d2fc26367f7e2d2be76da
C팀 <sywon2005@gmail.com>
2023-07-18 21:47:27 +0900

    빈 결과값 페이지

---
+++ component/EmptySeacrchResult.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const messages = {
+  NOT_FOUND: '검색 결과가 없습니다.',
+  EMPTY_KEYWORD: '검색어를 입력하세요',
+  NO_LIST: '게시글이 없습니다.',
+};
+
+const EmptySeacrchResult = ({type}) => {
+  return (
+    <View style={styles.block}>
+      <Text style={styles.text}>{messages[type]}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#9e9e9e',
+    fontSize: 16,
+  },
+});
+
+export default EmptySeacrchResult;
